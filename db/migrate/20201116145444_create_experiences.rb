099b66de86b0811f3a2186c5152a7ee8408c8d2d
jirou6699 <shundom09@gmail.com>
2020-11-17 00:11:05 +0900

    Add-model-experience

---
+++ db/migrate/20201116145444_create_experiences.rb
@@ -0,0 +1,16 @@
+class CreateExperiences < ActiveRecord::Migration[6.0]
+  def change
+    create_table :experiences do |t|
+      t.string    :company
+      t.string    :position
+      t.string    :title
+      t.text      :text
+      t.integer   :price_id
+      t.string    :talk_time
+      t.string    :industry_id
+      t.string    :occupation_id
+      t.integer   :prefecture_id
+      t.timestamps
+    end
+  end
+end
